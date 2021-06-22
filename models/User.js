b92907633bd8c8f5bd240e26712634e5523f1d1a
Sara <sara.m.adamski@gmail.com>
2021-06-22 15:23:09 -0500

    assign status id a default value of 1 (To Do), require username to be unique, add handlebars files for dashboard and single project

--- models/User.js
+++ models/User.js
@@ -14,7 +14,8 @@ User.init(
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
