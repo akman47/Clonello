225108de92b8d29690ee9b9b8ad9cf3cd0d36359
Sara <sara.m.adamski@gmail.com>
2021-06-21 20:41:13 -0500

    connect to server

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection.js');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'I will never tell!',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// app.use(routes);
+
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
